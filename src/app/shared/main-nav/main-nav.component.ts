762ee5912c210afce0f9b59ef0a1eb98cfb80945
nepex <thechump@hotmail.com>
2019-05-11 22:35:09 -0500

    interactive menu

--- src/app/shared/main-nav/main-nav.component.ts
+++ src/app/shared/main-nav/main-nav.component.ts
@@ -1,12 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
     selector: 'prf-main-nav',
     templateUrl: 'main-nav.component.html',
-    styleUrls: ['./main-nav.component.scss']
+    styleUrls: ['./main-nav.component.scss'],
+    animations: [
+        trigger('showHide', [
+            state('show', style({
+                top: '0px'
+            })),
+            state('hide', style({
+                top: '-42px'
+            })),
+            transition('show => hide', [
+                animate('.25s')
+            ]),
+            transition('hide => show', [
+                animate('.25s')
+            ]),
+        ]),
+        trigger('createDestroy', [
+            state('create', style({
+                display: 'block'
+            })),
+            state('destroy', style({
+                display: 'none'
+            })),
+            transition('create => destroy', [
+                animate('.25s')
+            ]),
+            transition('destroy => create', [
+                animate('.0s')
+            ]),
+        ]),
+    ]
 })
 
 export class MainNavComponent implements OnInit {
+    @Input() selectedTab: string;
+    isCollapsed: boolean = true;
+    showMenu: boolean = true;
+
     constructor() { }
 
     ngOnInit() { }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,25 @@
 import { NgModule } from '@angular/core';
 import { MainNavComponent } from './main-nav/main-nav.component';
+import { CommonModule } from '@angular/common';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { OffClickDirective } from './off-click-directive/off-click.directive';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @NgModule({
-    imports: [],
-    exports: [MainNavComponent],
-    declarations: [MainNavComponent],
+    imports: [
+        CommonModule,
+        NgbModule,
+        BrowserAnimationsModule
+    ],
+    exports: [
+        MainNavComponent,
+        OffClickDirective
+    ],
+    declarations: [
+        MainNavComponent,
+        OffClickDirective
+    ],
     providers: [],
 })
 export class SharedModule { }
